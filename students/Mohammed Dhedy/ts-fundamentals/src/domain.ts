913f9efbd8a4f46a775ee0ca80e96369a9e10d98
mohammed dhedy <mhmddhedy1234@gmail.com>
2026-09-07 02:46:46 +0300

    feat : add TaskSummary type

--- students/Mohammed Dhedy/ts-fundamentals/src/domain.ts
+++ students/Mohammed Dhedy/ts-fundamentals/src/domain.ts
@@ -1,28 +1,37 @@
-export type Priority = "LOW"|"MEDIUM"|"HIGH";
-export type Role = "USER"|"ADMIN";
+export type Priority = "LOW" | "MEDIUM" | "HIGH";
+export type Role = "USER" | "ADMIN";
 
-export interface User{
-    id:number;
-    username:string;
-    email:string;
-    displayName?:string;
-    role?:Role;
-    createdAt:string;
+export interface User {
+  id: number;
+  username: string;
+  email: string;
+  displayName?: string;
+  role?: Role;
+  createdAt: string;
 }
-export interface Status{
-    id:number;
-    name:string;
-    position:number;
-    color?:string;
+export interface Status {
+  id: number;
+  name: string;
+  position: number;
+  color?: string;
 }
-export interface Task{
-    id:number;
-    title:string;
-    description?:string;
-    status:Status;
-    createdBy:User;
-    priority:Priority;
-    targetDate:string;
-    createdAt:string;
-    updatedAt:string;
+export interface Task {
+  id: number;
+  title: string;
+  description?: string;
+  status: Status;
+  createdBy: User;
+  priority: Priority;
+  targetDate: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+type TaskSummary = Pick<Task, "id" | "title" | "priority">;
+export function toSummary(t: Task): TaskSummary {
+  return {
+    id: t.id,
+    title: t.title,
+    priority: t.priority,
+  };
 }
