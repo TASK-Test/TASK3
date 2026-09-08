a7f3c136c9a2ef115ada71ddab8e82fdd5de43df
mohammed dhedy <mhmddhedy1234@gmail.com>
2026-09-08 21:48:47 +0300

    chore: rename spring boot project

--- students/Mohammed Dhedy/backend/TaskTracker/src/main/java/com/example/tasktracker/TaskTrackerApplication.java
+++ students/Mohammed Dhedy/backend/TaskTracker/src/main/java/com/example/tasktracker/TaskTrackerApplication.java
@@ -1,14 +1,14 @@
-package com.example.demo;
+package com.example.tasktracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-public class DemoApplication {
+public class TaskTrackerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(TaskTrackerApplication.class, args);
 	}
 
 }
