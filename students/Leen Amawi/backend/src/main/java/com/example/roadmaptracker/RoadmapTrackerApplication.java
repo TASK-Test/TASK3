dbe3c145d2c69bb9a788069ee7843cfcd7c7e49f
Leen Amawi <aleen7728@gmail.com>
2026-09-09 13:56:56 +0300

    feat: add database startup test

--- students/Leen Amawi/backend/src/main/java/com/example/roadmaptracker/RoadmapTrackerApplication.java
+++ students/Leen Amawi/backend/src/main/java/com/example/roadmaptracker/RoadmapTrackerApplication.java
@@ -1,12 +1,22 @@
 package com.example.roadmaptracker;
-
+import com.example.roadmaptracker.entity.User;
+import com.example.roadmaptracker.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RoadmapTrackerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RoadmapTrackerApplication.class, args);
+    }
+    @Bean
+    CommandLineRunner run(UserRepository userRepository) {
+        return args -> {
+            User user = userRepository.save(new User(1,"Leen"));
+            System.out.println("Saved user: " + user.getId() + " - " + user.getName());
+        };
     }
 }
