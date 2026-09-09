b92da923762f6745b317df02151596dafd71c06d
Leen Amawi <aleen7728@gmail.com>
2026-09-09 13:56:52 +0300

    feat: add user repository

--- students/Leen Amawi/backend/src/main/java/com/example/roadmaptracker/repository/UserRepository.java
+++ students/Leen Amawi/backend/src/main/java/com/example/roadmaptracker/repository/UserRepository.java
@@ -1,4 +1,7 @@
 package com.example.roadmaptracker.repository;
+import com.example.roadmaptracker.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public class UserRepository {
+public interface UserRepository extends JpaRepository<User, Long> {
+
 }
