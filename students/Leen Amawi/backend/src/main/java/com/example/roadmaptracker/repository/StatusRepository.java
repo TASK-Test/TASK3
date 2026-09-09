12304197b311725a5fe08bfc63e381715dfaff35
Leen Amawi <aleen7728@gmail.com>
2026-09-09 16:59:28 +0300

    feat: add priority enum

---
+++ students/Leen Amawi/backend/src/main/java/com/example/roadmaptracker/repository/StatusRepository.java
@@ -0,0 +1,4 @@
+package com.example.roadmaptracker.repository;
+
+public class StatusRepository {
+}
---
+++ students/Leen Amawi/backend/src/main/java/com/example/roadmaptracker/repository/TaskRepository.java
@@ -0,0 +1,4 @@
+package com.example.roadmaptracker.repository;
+
+public interface TaskRepository {
+}
