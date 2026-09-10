e4def31cda83538466ca5a468dd695312e1f546a
mohammed dhedy <mhmddhedy1234@gmail.com>
2026-09-11 02:57:12 +0300

    fix: remove obsolete user startup test

--- students/Mohammed Dhedy/backend/TaskTracker/src/main/java/com/example/tasktracker/TaskTrackerApplication.java
+++ students/Mohammed Dhedy/backend/TaskTracker/src/main/java/com/example/tasktracker/TaskTrackerApplication.java
@@ -2,9 +2,6 @@ package com.example.tasktracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import com.example.tasktracker.entity.User;
-import com.example.tasktracker.repository.UserRepository;
 
 @SpringBootApplication
 public class TaskTrackerApplication {
@@ -12,13 +9,5 @@ public class TaskTrackerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TaskTrackerApplication.class, args);
 	}
-@Bean
-CommandLineRunner test(UserRepository repository){
-	return args->{
-		User user=new User();
-		user.setName("mohammed");
-		repository.save(user);
-		System.out.println("user is : " +user.getId() + " "+ user.getName());
-	};
-}
+
 }
--- students/Mohammed Dhedy/backend/TaskTracker/src/main/java/com/example/tasktracker/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.tasktracker.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.tasktracker.entity.User;
-
-public interface UserRepository extends JpaRepository<User,Long>{
-
-}
