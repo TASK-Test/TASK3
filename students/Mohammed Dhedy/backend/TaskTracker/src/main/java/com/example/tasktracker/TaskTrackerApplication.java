be6cd331218d2c7978fe504519d3fec5ea6d410e
mohammed dhedy <mhmddhedy1234@gmail.com>
2026-09-10 13:16:50 +0300

    feat: add User/Status/Task entities and flyway schema

--- students/Mohammed Dhedy/backend/TaskTracker/src/main/java/com/example/tasktracker/TaskTrackerApplication.java
+++ students/Mohammed Dhedy/backend/TaskTracker/src/main/java/com/example/tasktracker/TaskTrackerApplication.java
@@ -2,7 +2,6 @@ package com.example.tasktracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import com.example.tasktracker.entity.User;
 import com.example.tasktracker.repository.UserRepository;
@@ -13,13 +12,5 @@ public class TaskTrackerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TaskTrackerApplication.class, args);
 	}
-@Bean
-CommandLineRunner test(UserRepository repository){
-	return args->{
-		User user=new User();
-		user.setName("mohammed");
-		repository.save(user);
-		System.out.println("user is : " +user.getId() + " "+ user.getName());
-	};
-}
+
 }
