f4bd244d50abe2e9346fc7007dde65d49bedea8d
LeenAmawi <aleen7728@gmail.com>
2026-09-13 16:19:28 +0300

    Leen/chore/scaffold frontend (#47)
    * chore: scaffold frontend
    
    * add a Vite dev-server proxy

---
+++ students/Leen Amawi/frontend/src/components/Header.tsx
@@ -0,0 +1,9 @@
+function Header(){
+    return (
+        <header>
+            <h1>Task Tracker</h1>
+            <h2>Manage your tasks easily</h2>
+        </header>
+    )
+}
+export default Header
