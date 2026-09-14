02903213ce626f0abf8ebf640554acfe00d552f7
mohammed dhedy <mhmddhedy1234@gmail.com>
2026-09-14 13:23:35 +0300

    feat : add filters with status , sort by target date

--- students/Mohammed Dhedy/frontend/src/App.tsx
+++ students/Mohammed Dhedy/frontend/src/App.tsx
@@ -8,14 +8,43 @@ import FilterBar from "./components/FilterBar/FilterBar";
 function App() {
   const [mockTasks] = useState<Task[]>(tasks);
   const [search, setSearch] = useState<string>("");
+  const [statusFilter, setStatusFilter] = useState<string>("all");
+  const [targetDateFilter, setTargetDateFilter] = useState<string>("none");
   let results: Task[] = [...mockTasks];
   if (search.length > 0) {
-    results = mockTasks.filter((t) => t.title.toLowerCase().includes(search.toLowerCase()));
+    results = mockTasks.filter((t) =>
+      t.title.toLowerCase().includes(search.toLowerCase()),
+    );
+  }
+  if (statusFilter !== "all") {
+    results = results.filter((t) => t.status.name === statusFilter);
+  }
+  if (targetDateFilter !== "none") {
+    if (targetDateFilter.match("asc")) {
+      results = results.sort(
+        (a, b) =>
+          new Date(a.targetDate || "").getTime() -
+          new Date(b.targetDate || "").getTime(),
+      );
+    } else {
+      results = results.sort(
+        (a, b) =>
+          new Date(b.targetDate || "").getTime() -
+          new Date(a.targetDate || "").getTime(),
+      );
+    }
   }
   return (
     <>
       <Header />
-      <FilterBar setSearch={setSearch} search={search} />
+      <FilterBar
+        setSearch={setSearch}
+        search={search}
+        statusFilter={statusFilter}
+        setStatusFilter={setStatusFilter}
+        targetDateFilter={targetDateFilter}
+        setTargetDateFilter={setTargetDateFilter}
+      />
       <TaskList tasks={results} />
     </>
   );
--- students/Mohammed Dhedy/frontend/src/components/FilterBar/FilterBar.tsx
+++ students/Mohammed Dhedy/frontend/src/components/FilterBar/FilterBar.tsx
@@ -1,17 +1,32 @@
 import styles from "./FilterBar.module.css";
 type filterBarProps = {
-  setSearch: React.Dispatch<React.SetStateAction<string>>;
   search: string;
+  statusFilter: string;
+  targetDateFilter: string;
+  setSearch: React.Dispatch<React.SetStateAction<string>>;
+  setStatusFilter: React.Dispatch<React.SetStateAction<string>>;
+  setTargetDateFilter: React.Dispatch<React.SetStateAction<string>>;
 };
-const FilterBar = ({ search, setSearch }: filterBarProps) => {
+const FilterBar = (props: filterBarProps) => {
   return (
     <div className={styles.filter_bar}>
       <input
         placeholder="search a task"
         type="search"
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
+        value={props.search}
+        onChange={(e) => props.setSearch(e.target.value)}
       />
+      <select value={props.statusFilter} onChange={(e)=>props.setStatusFilter(e.target.value)} className={styles.dropdown_menu}>
+        <option value="all">filter by status(all)</option>
+        <option value="BackLog">backlog</option>
+        <option value="In Progress">in progres</option>
+        <option value="Done">done</option>
+      </select>
+      <select value={props.targetDateFilter} onChange={(e)=>props.setTargetDateFilter(e.target.value)} className={styles.dropdown_menu}>
+        <option value="none">sort(none)</option>
+        <option value="asc">Asc</option>
+        <option value="desc">Desc</option>
+      </select>
     </div>
   );
 };
