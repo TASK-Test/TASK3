d92e0e323a8c22840726bae96c9129a7e365f769
mohammed dhedy <mhmddhedy1234@gmail.com>
2026-09-14 23:19:00 +0300

    feat : fetch tasks from backend insted of mockTasks

---
+++ students/Mohammed Dhedy/frontend/src/api/client.ts
@@ -0,0 +1,18 @@
+const BASE_URL: string = "/api";
+import type { Task } from "../types/task";
+
+const getTasks = async (): Promise<Task[]> => {
+  const response = await fetch(`${BASE_URL}/tasks`);
+  if (!response.ok) {
+    throw new Error("something went wrong :(");
+  }
+  return response.json();
+};
+const getTask = async (id: number): Promise<Task> => {
+  const response = await fetch(`${BASE_URL}/tasks/${id}`);
+  if (!response.ok) {
+    throw new Error("something went wrong");
+  }
+  return response.json();
+};
+export { getTasks, getTask };
--- students/Mohammed Dhedy/frontend/src/components/TaskDetails/TaskDetails.tsx
+++ students/Mohammed Dhedy/frontend/src/components/TaskDetails/TaskDetails.tsx
@@ -1,17 +1,45 @@
 import styles from "./TaskDetails.module.css";
 
 import { useNavigate, useParams } from "react-router-dom";
-import { tasks } from "../../mock/tasks";
 import type { Task } from "../../types/task";
 import PriorityBadge from "../Badges/PriorityBadge/PriorityBadge";
 import StatusChip from "../Badges/StatusChip/StatusChip";
+import { useEffect, useState } from "react";
+import { getTask } from "../../api/client";
 const TaskDetails = () => {
+  const [task, setTask] = useState<Task>();
+  const [error, setError] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(true);
+
   const { taskId } = useParams();
   const navigate = useNavigate();
-  const task: Task | undefined = tasks.find((t) => t.id == Number(taskId));
+  useEffect(() => {
+    let ignore = false;
+    const getRes = async () => {
+      try {
+        setLoading(true);
+        setError("");
+        const taskRes: Task = await getTask(Number(taskId));
+        if (!ignore) setTask(taskRes);
+      } catch (error) {
+        if (!ignore)
+          setError(error instanceof Error ? error.message : "unexpected error");
+      } finally {
+        if (!ignore) setLoading(false);
+      }
+    };
+    getRes();
+    return () => {
+      ignore = true;
+    };
+  }, [taskId]);
   return (
     <>
-      {task !== undefined ? (
+      {loading ? (
+        <p className={styles.warning}>loading ... </p>
+      ) : error ? (
+        <p className={styles.warning}>{error}</p>
+      ) : task !== undefined ? (
         <>
           <p onClick={() => navigate(-1)} className={styles.back}>
             Back To Tasks -{">"}
@@ -20,7 +48,7 @@ const TaskDetails = () => {
             <span>Task #{taskId}</span>
             <div className={styles.badges}>
               <PriorityBadge priority={task.priority} />
-              <StatusChip name={task.status.name} color={task.status.color}/>
+              <StatusChip name={task.status.name} color={task.status.color} />
             </div>
             <h2>Task title : </h2>
             <p className={styles.info}>{task.title}</p>
