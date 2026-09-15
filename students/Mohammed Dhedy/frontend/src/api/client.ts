765a274fedc1c9131b558cd7c79565154a4197ba
mohammed dhedy <mhmddhedy1234@gmail.com>
2026-09-15 18:03:26 +0300

    feat(api): add status fetching and task request types

--- students/Mohammed Dhedy/frontend/src/api/client.ts
+++ students/Mohammed Dhedy/frontend/src/api/client.ts
@@ -1,5 +1,5 @@
 const BASE_URL: string = "/api";
-import type { Task } from "../types/task";
+import type { Status, Task } from "../types/task";
 
 const getTasks = async (): Promise<Task[]> => {
   const response = await fetch(`${BASE_URL}/tasks`);
@@ -17,4 +17,11 @@ const getTask = async (id: number): Promise<Task> => {
   }
   return response.json();
 };
-export { getTasks, getTask };
+const getStatuses = async (): Promise<Status[]> => {
+  const response = await fetch(`${BASE_URL}/statuses`);
+  if (!response.ok) {
+    throw new Error("could not create task");
+  }
+  return response.json();
+};
+export { getTasks, getTask, getStatuses };
