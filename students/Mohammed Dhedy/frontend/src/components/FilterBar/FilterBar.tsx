2c1584beb41b543cb1b516850c12d8894e0987d2
mohammed dhedy <mhmddhedy1234@gmail.com>
2026-09-16 18:14:21 +0300

    feat : make filter bar show statuses from backend insted of static list

--- students/Mohammed Dhedy/frontend/src/components/FilterBar/FilterBar.tsx
+++ students/Mohammed Dhedy/frontend/src/components/FilterBar/FilterBar.tsx
@@ -1,4 +1,8 @@
+import { useEffect, useState } from "react";
 import styles from "./FilterBar.module.css";
+import type { Status } from "../../types/task";
+import { getStatuses } from "../../api/client";
+import QuickMessage from "../QuickMessaage/QuickMessage";
 type filterBarProps = {
   search: string;
   statusFilter: string;
@@ -8,26 +12,54 @@ type filterBarProps = {
   setTargetDateFilter: React.Dispatch<React.SetStateAction<string>>;
 };
 const FilterBar = (props: filterBarProps) => {
+  const [statuses, setStatuses] = useState<Status[]>([]);
+  const [error, setError] = useState<string>("");
+  useEffect(() => {
+    const getRes = async () => {
+      try {
+        const response: Status[] = await getStatuses();
+        setStatuses(response);
+        if (response.length <= 0) throw new Error("There is no statuses !");
+      } catch (error) {
+        setError(error instanceof Error ? error.message : "");
+      }
+    };
+    getRes();
+  }, []);
   return (
-    <div className={styles.filter_bar}>
-      <input
-        placeholder="search a task"
-        type="search"
-        value={props.search}
-        onChange={(e) => props.setSearch(e.target.value)}
-      />
-      <select value={props.statusFilter} onChange={(e)=>props.setStatusFilter(e.target.value)} className={styles.dropdown_menu}>
-        <option value="all">filter by status(all)</option>
-        <option value="Backlog">backlog</option>
-        <option value="In Progress">in progres</option>
-        <option value="Done">done</option>
-      </select>
-      <select value={props.targetDateFilter} onChange={(e)=>props.setTargetDateFilter(e.target.value)} className={styles.dropdown_menu}>
-        <option value="none">sort(none)</option>
-        <option value="asc">Asc</option>
-        <option value="desc">Desc</option>
-      </select>
-    </div>
+    <>
+      {error.length > 0 && <QuickMessage type="error" message={error} />}
+      <div className={styles.filter_bar}>
+        <input
+          placeholder="search a task"
+          type="search"
+          value={props.search}
+          onChange={(e) => props.setSearch(e.target.value)}
+        />
+        <select
+          disabled={error.length > 0}
+          value={props.statusFilter}
+          onChange={(e) => props.setStatusFilter(e.target.value)}
+          className={styles.dropdown_menu}
+        >
+          <option value="all">filter by status(all)</option>
+          {statuses.map((s) => (
+            <option key={s.id} value={s.name}>
+              {s.name}
+            </option>
+          ))}
+        </select>
+        <select
+          value={props.targetDateFilter}
+          onChange={(e) => props.setTargetDateFilter(e.target.value)}
+          className={styles.dropdown_menu}
+        >
+          <option value="none">sort(none)</option>
+          <option value="asc">Asc</option>
+          <option value="desc">Desc</option>
+        </select>
+      </div>
+    </>
   );
 };
 export default FilterBar;
