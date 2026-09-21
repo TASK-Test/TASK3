9ea74731e5ab1d459d65eef129ea4b4a95d7c38c
mohammed dhedy <mhmddhedy1234@gmail.com>
2026-09-21 22:35:04 +0300

    feat : add client api Error with respnse handler

---
+++ students/Mohammed Dhedy/frontend/src/api/ApiError.ts
@@ -0,0 +1,22 @@
+export type ApiErrorBody = {
+  timestamp: string;
+  status: number;
+  error: string;
+  message: string;
+  path: string;
+  fieldErrors?: Record<string, string>;
+};
+
+export class ApiError extends Error {
+  status: number;
+  fieldErrors?: Record<string, string>;
+  constructor(
+    status: number,
+    message: string,
+    fieldErrors?: Record<string, string>,
+  ) {
+    super(message);
+    this.status = status;
+    this.fieldErrors = fieldErrors;
+  }
+}
