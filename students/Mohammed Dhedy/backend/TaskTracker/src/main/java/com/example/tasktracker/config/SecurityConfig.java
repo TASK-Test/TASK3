24adfce24d89938ff446097e37654072a149fe35
mohammed dhedy <mhmddhedy1234@gmail.com>
2026-09-23 23:13:07 +0300

    feat : add authenticationManager

--- students/Mohammed Dhedy/backend/TaskTracker/src/main/java/com/example/tasktracker/config/SecurityConfig.java
+++ students/Mohammed Dhedy/backend/TaskTracker/src/main/java/com/example/tasktracker/config/SecurityConfig.java
@@ -2,8 +2,12 @@ package com.example.tasktracker.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -26,4 +30,14 @@ public class SecurityConfig {
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    AuthenticationManager authenticationManager(
+            UserDetailsService userDetailsService,
+            PasswordEncoder passwordEncoder) {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder);
+        return new ProviderManager(provider);
+    }
+
 }
