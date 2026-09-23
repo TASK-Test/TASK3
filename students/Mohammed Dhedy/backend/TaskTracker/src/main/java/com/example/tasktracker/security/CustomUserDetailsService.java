65382e3f17a07d8af7b2bdeb40443402fe933818
mohammed dhedy <mhmddhedy1234@gmail.com>
2026-09-23 22:15:09 +0300

    feat : add customeUserDetailsService

---
+++ students/Mohammed Dhedy/backend/TaskTracker/src/main/java/com/example/tasktracker/security/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.example.tasktracker.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.tasktracker.entity.User;
+import com.example.tasktracker.repository.UserRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("user name " + username + " not found"));
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPasswordHash())
+                .roles(user.getRole().name())
+                .build();
+    }
+
+}
