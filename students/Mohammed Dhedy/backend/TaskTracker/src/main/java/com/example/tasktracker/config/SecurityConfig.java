05eebf08d2e084fc93ff02636f3106c5d3929136
mohammed dhedy <mhmddhedy1234@gmail.com>
2026-09-24 21:38:55 +0300

    feat : authenticate requests with jwt filter

--- students/Mohammed Dhedy/backend/TaskTracker/src/main/java/com/example/tasktracker/config/SecurityConfig.java
+++ students/Mohammed Dhedy/backend/TaskTracker/src/main/java/com/example/tasktracker/config/SecurityConfig.java
@@ -11,17 +11,23 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.example.tasktracker.security.JwtAuthFilter;
+import com.example.tasktracker.security.JwtAuthenticationEntryPoint;
 
 @Configuration
 public class SecurityConfig {
     @Bean
-    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http, JwtAuthFilter jwtAuthFilter,JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint) throws Exception {
         http.csrf(csrf -> csrf.disable())
                 .cors(cors -> {
                 })
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .exceptionHandling(exception->exception.authenticationEntryPoint(jwtAuthenticationEntryPoint))
                 .authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/**").permitAll()
                         .anyRequest().authenticated());
+        http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
